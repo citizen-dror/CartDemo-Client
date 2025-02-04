4e5add135bfd7874c75fc69e715c38429b104214
citizen-dror <reshef.dror@gmail.com>
2025-02-04 07:40:36 +0200

    chnage tilte, connection to prod

--- src/mocks/itemsMock.ts
+++
@@ -1,7 +0,0 @@
-import { Item } from "../types/cartTypes";
-
-export const itemsMock: Item[] = [
-  { id: "1", name: "Milk", category: "Dairy", price: 2.0 },
-  { id: "2", name: "Cheese", category: "Dairy", price: 3.5 },
-  { id: "3", name: "Apple", category: "Fruits", price: 1.2 }
-];
